0e4ff31cf28aeb5d1ad17f8b5a06bc75687191d0
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2025-08-14 09:48:15 +0530

    Added dashboard middleware

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authMiddleware = require('./middleware/auth');
+const dashboardRoute = require('../../../dashboard');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Home Page - No Auth Needed');
 });
-app.use('/dashboard', authMiddleware);
+app.use('/dashboard', authMiddleware, dashboardRoute);
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
